39a0279938b856b4c5cf849c2d796f9eddbb6bff
hora-keven <kevensantos@gmail.com>
2023-09-25 10:52:01 -0300

    update

--- ProjetoAPI/ApiLivros.py
+++ ProjetoAPI/ApiLivros.py
@@ -1,40 +1,28 @@
 import fastapi as f
-from fastapi import Depends, HTTPException, status, Response
+from fastapi import HTTPException, status
 import uvicorn
-import json
-import requests
 import LivrosB
-from LivrosB import FuncaoBanco, Livros, LivroBase, Atualiza_livro
-from sqlalchemy import select, insert, update, delete
-from sqlalchemy.orm import Session, sessionmaker
+from LivrosB import FuncaoBanco, Livros
+from Models import  LivroBase, Atualiza_livro
+
 app = f.FastAPI()
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=LivrosB.engine)
-session = LivrosB.session
 cursor = LivrosB.cursor
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @app.get("/livros", response_model=None, status_code=status.HTTP_201_CREATED)
 async def selecionar() -> dict:
     livros = FuncaoBanco.selecionar_tudo(Livros)
     return livros
 
-
 @app.get("/livros/{id}")  
-def selecionar_livro_id(id) -> str:
+def selecionar_livro_id(id) -> dict:
     buscando = FuncaoBanco.selecionar_por_id(id)
+    if buscando == "id não encontrado":
+        raise HTTPException(status_code=404, detail="Livro não encontrado com esse id") 
     return buscando
 
-@app.post("/cadastra_livro", tags=['Livro'])
+
+@app.post("/cadastra_livro", tags=['Livro'],status_code=status.HTTP_201_CREATED)
 def cadastrar_livro(livro:LivroBase) -> dict:
-    
     livro_dic = {
         'nome_livro':livro.nome_livro,
         'quantidade_livro':livro.quantidade_livro
@@ -42,15 +30,25 @@ def cadastrar_livro(livro:LivroBase) -> dict:
     FuncaoBanco.adicionar_dados_banco(livro)
     return livro_dic
 
-@app.put("/atualiza_livro/{id}", response_model=None, status_code=status.HTTP_201_CREATED)
-async def cadastrar_livro(id:int, atualiza:Atualiza_livro = None) -> dict:
+
+@app.put("/atualiza_livro/{id}", tags=["Livros"], response_model=None, status_code=status.HTTP_201_CREATED)
+async def cadastrar_livro(id:int, atualiza:Atualiza_livro) -> dict:
+    selecionado = FuncaoBanco.selecionar_por_id(id)
+    atualiza.id = id
+    FuncaoBanco.atualizar(atualiza)
     
-    livros = FuncaoBanco.selecionar_por_id(id)
-    atualizando = FuncaoBanco.atualizar(atualiza)
+    if selecionado == f"id não encontrado":
+        raise HTTPException(status_code=404, detail="Livro não encontrado com esse id")
+    return selecionado
 
-    return atualizando
 
-
+@app.delete("/deleta_livro/{id}")
+async def deletar_livro(id:int) ->  str:
+      deletando = FuncaoBanco.deletar_por_id(id)
+      if deletando =="id não encontrado":
+            raise HTTPException(status_code=404, detail="Livro não encontrado com esse id")
+      
+      return deletando
 
 if __name__ == '__main__':
     uvicorn.run(app)
--- ProjetoAPI/LivrosB.py
+++ ProjetoAPI/LivrosB.py
@@ -1,39 +1,22 @@
-from sqlalchemy import Integer, String, create_engine, update, delete, select, bindparam
-from sqlalchemy.orm import Mapped,mapped_column,DeclarativeBase, Session
-from sqlalchemy.orm import Session
-from pydantic import BaseModel
-from typing import Optional
+from sqlalchemy import Integer, String, create_engine, update, delete, select
+from sqlalchemy.orm import Mapped,mapped_column,DeclarativeBase
+from Models import Base, LivroBase
 import json
 import requests
 from sqlalchemy.dialects.sqlite import insert
 
+
 engine = create_engine("sqlite:///Livros.db", echo=True)
 cursor = engine.connect()
-session = Session(engine)
 
-class LivroBase(BaseModel):
-   id:Optional[int]=None
-   nome_livro:str
-   quantidade_livro:int
-   ...
-
-class Atualiza_livro(BaseModel):
-    id:Optional[int]=None
-    nome_livro: Optional[str] = None
-    quantidade_livro: Optional[int] = None
-
-class Base(DeclarativeBase):
-  ...
 class Livros(Base):
     __tablename__ = 'Livros'
     id:Mapped[int] = mapped_column(primary_key=True, nullable=True)
     nome_livro:Mapped[str]=mapped_column(String(100))
     quantidade_livro:Mapped[int]=mapped_column(Integer)
     
-
     
 class FuncaoBanco:
-   
     @staticmethod
     def adicionar_dados_banco(livro:Livros) -> None:
         cursor.execute(insert(Livros),[
@@ -41,70 +24,87 @@ class FuncaoBanco:
         ])
         cursor.commit()
       
-      
     @staticmethod
     def selecionar_tudo(livro:Livros)->list:
-        # cursor.execute(select(Livros).where(livro.id==1))
-        # cursor.commit()
         selecionando = cursor.execute(select(Livros), [{'nome_livro':livro.nome_livro}])
         livroZip = []
-
         coluna = ['Id', 'nome_livro', 'quantidade_capitulos']
        
-
         for linha in selecionando:
             livroZip.append(dict(zip(coluna, linha)))
          
         return livroZip
 
     @staticmethod
-    def selecionar_por_id(id:int)->str:
-       
+    def selecionar_por_id(id:int)->dict or str:
         dados = cursor.execute(select(Livros).filter(Livros.id == id)).all()
         print(dados)
 
-        dados = [{"id":dados[0][0],
-                  "nome":dados[0][1], 
-                  "quantidade capitulos":dados[0][2]}]
+        if dados == []:
         
-    
+           return f"id não encontrado"
         
-        return f'{dados}'
+        dados ={
+                "id":dados[0][0],
+                "nome_livro":dados[0][1], 
+                "quantidade_livro":dados[0][2]
+                }
+        
+        return dados
     
-
     @staticmethod
     def atualizar(livro:LivroBase)->str:
-     
-         
-        dados_atualizando = cursor.execute(update(Livros).where(Livros.id == livro.id),
-                  [{ "nome_livro": livro.nome_livro, "quantidade_livro":livro.quantidade_livro},
-        ])
+        livros = cursor.execute(select(Livros), [{'nome_livro':Livros.nome_livro}]).fetchall()
+        mensagem = True
 
-        
-        print(dados_atualizando)
-        
+        for i, l in enumerate(livros):
+            if livro.id not in l:
+              mensagem = False
+          
+        if mensagem:
+            return f"{livro.id}"
+       
+        dados_atualizando = cursor.execute(update(Livros).where(Livros.id == livro.id),
+                [{"nome_livro": livro.nome_livro, "quantidade_livro":livro.quantidade_livro},
+        ])
+    
+        cursor.commit()
+
         
         return f'{dados_atualizando}'
+    
+    @staticmethod
+    def deletar_por_id(id:int) -> str:
+        livros = cursor.execute(select(Livros), [{'nome_livro':Livros.nome_livro}]).fetchall()
+        print(livros)
+        mensagem = True
+        for i, l in enumerate(livros):
+            if id not in l:
+                mensagem = False
 
+        if mensagem:
+           return "id não encontrado"
+        
+        cursor.execute(delete(Livros).where(Livros.id == id))
+        cursor.commit()
+        
+        return f"id: {id} deletado com sucesso!"
 
 
     def buscarDados(livros:list, capitulos:list):
-      #BUscando de uma api e populando o banco de dados
       request = requests.get("https://www.abibliadigital.com.br/api/books")
       j = json.loads(request.content)
       
       for i in range(66):
         livros.append(j[i]['name'])
         capitulos.append(j[i]['chapters'])
-      
+
       return livros, capitulos
     
 Base.metadata.create_all(engine) 
 
 
-l = Livros()
 
-l = LivroBase(id=67, nome_livro="o amanhã", quantidade_livro=25)
 
-FuncaoBanco.atualizar(l)
+
 
